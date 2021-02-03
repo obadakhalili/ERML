a65c7d5e286d0cf5d68cc6728d2877795e4c079c
Obada Khalili <54270856+obadakhalili@users.noreply.github.com>
2021-02-03 09:13:39 +0200

    Update valid identifier requirements

--- src/engine/parser/identifiers.ts
+++ src/engine/parser/identifiers.ts
@@ -3,7 +3,7 @@ const identifiers: string[] = []
 export function isValidIdentifier(identifier: string) {
   /** Valid identifier requirements
    * Maximum of 30 characters
-   * Starts with an uppercase/lowercase letter (a-z, A-Z)
+   * Starts with an uppercase/lowercase letter (a-z, A-Z), or an underscore
    * Every character after the first should either be a number, an underscore, or an uppercase/lowercase letter (a-z, A-Z)
    */
   return /^[a-zA-Z_]\w{0,29}$/.test(identifier)
