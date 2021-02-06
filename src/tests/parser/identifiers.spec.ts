164b3cc4edab7a3c2bb759759a9e1b4aac8b2049
Obada Khalili <obada.akhalili@gmail.com>
2021-02-06 11:05:17 +0200

    Refactor identifiers.spec.ts

--- src/tests/parser/identifiers.spec.ts
+++ src/tests/parser/identifiers.spec.ts
@@ -8,38 +8,38 @@ beforeAll(() => {
   isDuplicateIdentifier("User") // isDuplicateIdentifier inserts identifier if it's not a duplicate
 })
 
-describe("Tests for isValidIdentifier", () => {
-  it("Should return false", () => {
+describe("tests for isValidIdentifier", () => {
+  it("should return false", () => {
     expect(isValidIdentifier("1employee")).toBe(false)
   })
 
-  it("Should return false", () => {
+  it("should return false", () => {
     expect(isValidIdentifier("a very very very long identifier name")).toBe(
       false
     )
   })
 
-  it("Should return true", () => {
+  it("should return true", () => {
     expect(isValidIdentifier("Employee_table")).toBe(true)
   })
 })
 
-describe("Tests for isValidReference", () => {
-  it("Should return false", () => {
+describe("tests for isValidReference", () => {
+  it("should return false", () => {
     expect(isValidReference("a reference that doesn't exist")).toBe(false)
   })
 
-  it("Should return true", () => {
+  it("should return true", () => {
     expect(isValidReference("User")).toBe(true)
   })
 })
 
-describe("Tests for isNotDuplicate", () => {
-  it("Should return true", () => {
+describe("tests for isNotDuplicate", () => {
+  it("should return true", () => {
     expect(isDuplicateIdentifier("User")).toBe(true)
   })
 
-  it("Should return false", () => {
+  it("should return false", () => {
     expect(isDuplicateIdentifier("Translation")).toBe(false)
   })
 })
