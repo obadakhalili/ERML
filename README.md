647a836155f65d6c8c27c9a3b49ffb2c3f505449
Obada Khalili <obada.akhalili@gmail.com>
2021-02-12 14:03:37 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # ERML - Entity-Relationship Markup Language
 ERML is a super easy-to-write DSL that represents conceptual data models using the Entity-Relationship modeling concepts.
 What does that gibberish mean? read the [docs](https://erml.netlify.app/)
+
+# Installation and Usage
+
+You can install the ERML parser from [NPM](http://npmjs.org/package/erml) or [yarn](https://classic.yarnpkg.com/en/package/erml) via the commands
+
+```sh
+npm install erml # From NPM
+yarn add erml # From yarn
+```
+
+After that all you have to do is to execute the parser function exported by the library
+
+```js
+const ERMLParser = require("erml");
+const ast = ERMLParser(`ENTITY Example_entity { SIMPLE "attribute_name" }`);
+```
--- playground.ts
+++ playground.ts
@@ -1,85 +1,80 @@
 import tokenize from "./src/engine/lexer"
 import ERMLParser from "./src/engine/index"
 
+const ERMLCode = `ENTITY Employee {
+  PRIMARY "SSN",
+  SIMPLE "salary",
+  SIMPLE "DoB",
+  DERIVED "age",
+  COMPOSITE "full_name" {
+    SIMPLE "first_name",
+    SIMPLE "last_name"
+  }
+}
+
+ENTITY Department {
+  PRIMARY "name",
+  PRIMARY "number",
+  DERIVED "employees_number",
+  MULTIVALUED "locations"
+}
+
+ENTITY Project {
+  PRIMARY "number",
+  PRIMARY "name",
+  SIMPLE "location"
+}
+
+WEAK ENTITY Dependent OWNER Employee {
+  COMPOSITE "key" {
+    PARTIAL "name",
+    PARTIAL "DoB"
+  },
+  SIMPLE "relationship",
+  SIMPLE "gender"
+}
+
+REL Works_for {
+  Employee (1, N),
+  Department (20, N)
+}
+
+REL Manages {
+  Employee <PARTIAL, 1>,
+  Department <TOTAL, 1>,
+  ATTRIBUTES { SIMPLE "start_date" }
+}
+
+REL Supervision {
+  Employee <PARTIAL, 1>,
+  Employee <PARTIAL, N>
+}
+
+IDEN REL Dependents_of {
+  Employee <PARTIAL, 1>,
+  Dependent <TOTAL, N>
+}
+
+REL Works_on {
+  Employee <PARTIAL, N>,
+  Project <TOTAL, N>,
+  ATTRIBUTES { SIMPLE "hours" }
+}
+
+REL Controls {
+  Department <PARTIAL, N>,
+  Project <TOTAL, 1>
+}`
+
 try {
-  console.log(
+  // console.log(
     // tokenize(
-    ERMLParser(
-      `ENTITY Employee {
-        PRIMARY "SSN",
-        SIMPLE "salary",
-        SIMPLE "DoB",
-        DERIVED "age",
-        COMPOSITE "full_name" {
-          SIMPLE "first_name",
-          SIMPLE "last_name"
-        }
-      }
-      
-      ENTITY Department {
-        PRIMARY "name",
-        PRIMARY "number",
-        DERIVED "employees_number",
-        MULTIVALUED "locations"
-      }
-      
-      ENTITY Project {
-        PRIMARY "number",
-        PRIMARY "name",
-        SIMPLE "location"
-      }
-      
-      WEAK ENTITY Dependent OWNER Employee {
-        COMPOSITE "key" {
-          PARTIAL "name",
-          PARTIAL "DoB"
-        },
-        SIMPLE "relationship",
-        SIMPLE "gender"
-      }
-      
-      REL Works_for {
-        Employee (1, Infinity),
-        Department (20, Infinity)
-      }
-      
-      REL Manages {
-        Employee <PARTIAL, 1>,
-        Department <TOTAL, 1>,
-        ATTRIBUTES { SIMPLE "start_date" }
-      }
-      
-      REL Supervision {
-        Employee <PARTIAL, 1>,
-        Employee <PARTIAL, N>
-      }
-      
-      IDEN REL Dependents_of {
-        Employee <PARTIAL, 1>,
-        Dependent <TOTAL, N>
-      }
-      
-      REL Works_on {
-        Employee <PARTIAL, N>,
-        Project <TOTAL, N>,
-        ATTRIBUTES { SIMPLE "hours" }
-      }
-      
-      REL Controls {
-        Department <PARTIAL, N>,
-        Project <TOTAL, 1>
-      }`
-    ), ERMLParser(`ENTITY Employee {
-      PRIMARY "SSN",
-      SIMPLE "salary",
-      SIMPLE "DoB",
-      DERIVED "age",
-      COMPOSITE "full_name" {
-        SIMPLE "first_name",
-        SIMPLE "last_name"
-      }
-    }`)
-  )
+    const ast = ERMLParser(
+      ERMLCode
+    )
+  // )
+
+  console.log(ast)
 } catch (e) {
-  console.log(e.message)
+  console.log(e)
 }
