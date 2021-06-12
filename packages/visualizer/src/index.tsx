c62365b5d6e2313a6db584b3f192df22d987a5d2
Obada Khalili <obada.akhalili@gmail.com>
2021-06-12 19:09:55 +0300

    Visualizer: Setup

---
+++ packages/visualizer/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>ERML Visualizer</h1>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
