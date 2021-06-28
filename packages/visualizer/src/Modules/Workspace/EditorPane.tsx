ba389628419f9fa100cbf08939ba53598dd3ff0e
Obada Khalili <obada.akhalili@gmail.com>
2021-06-28 22:28:00 +0300

    Visualizer: Move controller bar into controller pane

--- packages/visualizer/src/Modules/Workspace/EditorPane.tsx
+++ packages/visualizer/src/Modules/Workspace/EditorPane.tsx
@@ -1,62 +1,27 @@
 import { useRecoilState } from "recoil"
-import { Select, ItemRenderer } from "@blueprintjs/select"
-import { MenuItem, Button } from "@blueprintjs/core"
 
-import SnippetExplorer from "./SnippetExplorer"
 import CodeMirror from "./CodeMirror"
-import {
-  activeViewerState,
-  activeSnippetState,
-  firstSnippetValueState,
-  ActiveViewer,
-} from "../../state"
-
-const ViewerSelect = Select.ofType<ActiveViewer>()
-
-const ViewerItem: ItemRenderer<ActiveViewer> = (
-  viewer,
-  { handleClick, modifiers }
-) => (
-  <MenuItem
-    key={viewer}
-    text={viewer}
-    active={modifiers.active}
-    onClick={handleClick}
-  />
-)
+import { activeSnippetState, firstSnippetValueState } from "../../state"
 
 export default function EditorPane() {
-  const [activeViewer, setActiveViewer] = useRecoilState(activeViewerState)
   const [activeSnippet, setActiveSnippet] = useRecoilState(activeSnippetState)
   const [firstSnippetValue, setFirstSnippetValue] = useRecoilState(
     firstSnippetValueState
   )
 
   return (
-    <>
-      <SnippetExplorer />
-      <ViewerSelect
-        filterable={false}
-        items={["Diagram", "AST"]}
-        activeItem={activeViewer}
-        onItemSelect={setActiveViewer}
-        itemRenderer={ViewerItem}
-      >
-        <Button text={activeViewer} rightIcon="double-caret-vertical" />
-      </ViewerSelect>
-      <CodeMirror
-        value={
-          activeSnippet?.value ||
-          firstSnippetValue ||
-          "// Code written here will be saved to newly created snippets"
-        }
-        options={{ lineWrapping: true, autoRefresh: { delay: 50 } }}
-        onBeforeChange={(editor, change, value) =>
-          activeSnippet
-            ? setActiveSnippet({ ...activeSnippet, value })
-            : setFirstSnippetValue(value)
-        }
-      />
-    </>
+    <CodeMirror
+      value={
+        activeSnippet?.value ||
+        firstSnippetValue ||
+        "// Code written here will be saved to newly created snippets"
+      }
+      options={{ lineWrapping: true, autoRefresh: { delay: 50 } }}
+      onBeforeChange={(editor, change, value) =>
+        activeSnippet
+          ? setActiveSnippet({ ...activeSnippet, value })
+          : setFirstSnippetValue(value)
+      }
+    />
   )
 }
--- packages/visualizer/src/Modules/Workspace/index.tsx
+++ packages/visualizer/src/Modules/Workspace/index.tsx
@@ -2,13 +2,17 @@ import { FC } from "react"
 import { RouteComponentProps } from "@reach/router"
 import SplitPane from "react-split-pane"
 
+import ControllerPane from "./ControllerPane"
 import EditorPane from "./EditorPane"
 import ViewerPane from "./ViewerPane"
 import "../../styles/react-split-pane.css"
 
 const Workspace: FC<RouteComponentProps> = () => (
-  <SplitPane defaultSize={350} minSize={350} maxSize={750}>
-    <EditorPane />
+  <SplitPane defaultSize={525} minSize={350} maxSize={600}>
+    <SplitPane allowResize={false} minSize={175}>
+      <ControllerPane />
+      <EditorPane />
+    </SplitPane>
     <ViewerPane />
   </SplitPane>
 )
